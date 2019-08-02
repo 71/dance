93faa2def56d91dcbc1c3e4fceb49ff0fd7a6c41
Grégoire Geis <git@gregoirege.is>
2019-08-02 11:02:37 +0200

    Started work on goto.

--- src/commands/goto.ts
+++ src/commands/goto.ts
@@ -1,4 +1,6 @@
 import * as vscode from 'vscode'
+import * as path   from 'path'
+import * as fs     from 'fs'
 
 import { registerCommand, Command, CommandDescriptor, CommandFlags, InputKind } from '.'
 
@@ -22,38 +24,75 @@ const jumps: [string, string][] = [
 function executeGoto(gotoType: number, editor: vscode.TextEditor, count: number, extend: boolean) {
   switch (gotoType) {
     case 0: // go to line start
-      editor.selections = editor.selections.map(x => x)
+      editor.selections = editor.selections.map(x =>
+        new vscode.Selection(extend ? x.anchor : x.active, new vscode.Position(x.active.line, 0)))
       break
+
     case 1: // go to line end
+      editor.selections = editor.selections.map(x => {
+        const line = editor.document.lineAt(x.active)
+
+        return new vscode.Selection(extend ? x.anchor : x.active, line.range.end)
+      })
       break
+
     case 2: // go to non-blank line start
       break
+
     case 3: // go to first line
     case 4: // go to first line
       break
+
     case 5: // go to last line
       break
+
     case 6: // go to last char of last line
       break
+
     case 7: // go to first displayed line
       break
+
     case 8: // go to middle displayed line
       break
+
     case 9: // go to last displayed line
       break
+
     case 10: // go to previous buffer
       break
+
     case 11: // go to file whose name is selected
-      break
+      const basePath = path.dirname(editor.document.fileName)
+
+      return new Promise<void>(resolve => fs.exists(basePath, exists => {
+        if (!exists)
+          return
+
+        let remaining = editor.selections.length
+
+        for (const selection of editor.selections) {
+          const filename = editor.document.getText(selection)
+          const filepath = path.resolve(basePath, filename)
+
+          fs.exists(filepath, exists => {
+            if (exists)
+              vscode.workspace.openTextDocument(filepath)
+
+            if (--remaining === 0)
+              resolve()
+          })
+        }
+      }))
+
     case 12: // go to last buffer modification position
       break
   }
 }
 
-registerCommand(Command.goto, CommandFlags.ChangeSelections, InputKind.ListOneItem, jumps, (editor, state, _, ctx) => {
+registerCommand(Command.goto, CommandFlags.ChangeSelections, InputKind.ListOneItem, jumps, (editor, state, _, __) => {
   return executeGoto(state.input, editor, state.currentCount, false)
 })
 
-registerCommand(Command.gotoExtend, CommandFlags.ChangeSelections, InputKind.ListOneItem, jumps, (editor, state, _, ctx) => {
+registerCommand(Command.gotoExtend, CommandFlags.ChangeSelections, InputKind.ListOneItem, jumps, (editor, state, _, __) => {
   return executeGoto(state.input, editor, state.currentCount, true)
 })
