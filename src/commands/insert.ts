2d1f30033fd5b1ab2760b03ecd70e84f09240252
Gregoire Geis <git@gregoirege.is>
2019-12-04 21:56:28 +0100

    Do not switch to insert mode after repeating edit (fixes #36).

--- src/commands/insert.ts
+++ src/commands/insert.ts
@@ -91,7 +91,6 @@ registerCommand(Command.repeatInsert, CommandFlags.Edit, async (editor, state, _
     return
 
   await CommandDescriptor.execute(ctx, editor, ...hist.commands[start])
-  await ctx.setMode(Mode.Insert)
 
   let end = i
 
