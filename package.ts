71fa19fe73dc07273cc5cab28c1eec1e4081a367
Gregoire Geis <git@gregoirege.is>
2019-12-04 22:27:42 +0100

    Bump version to 0.1.8.

--- package.ts
+++ package.ts
@@ -10,7 +10,7 @@ const pkg = {
   name: 'dance',
   displayName: 'Dance',
   description: 'Make those cursors dance.',
-  version: '0.1.7',
+  version: '0.1.8',
   license: 'MIT',
 
   publisher: 'gregoire',
