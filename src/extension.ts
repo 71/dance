dd9ed3bf044241d577ee930bdc70c4b977369f6c
Yuchen Shi <yuchenshi@google.com>
2020-02-19 15:47:40 -0800

    Keep selection empty at end of line but delete removes line break.

--- src/extension.ts
+++ src/extension.ts
@@ -434,15 +434,19 @@ export class Extension implements vscode.Disposable {
 
         const active = selection.active
 
-        if (active.character < editor.document.lineAt(active.line).range.end.character) {
-          // Move anchor to select 1 character after, but keep the cursor position.
-          normalizedSelections.push(new vscode.Selection(active.translate(0, 1), active));
-        } else if (active.character === 0) {
-          // Selection is on an empty line; we can't do much here.
+        if (active.character >= editor.document.lineAt(active.line).range.end.character) {
+          // Selection is at line end. Just keep it that way.
           normalizedSelections.push(selection);
         } else {
-          // Selection is at the very end of the line. Select the last character instead.
-          normalizedSelections.push(new vscode.Selection(active, active.translate(0, -1)));
+          const offset = editor.document.offsetAt(selection.active)
+          const nextPos = editor.document.positionAt(offset + 1)
+          if (nextPos.isAfter(selection.active)) {
+            // Move anchor to select 1 character after, but keep the cursor position.
+            normalizedSelections.push(new vscode.Selection(active.translate(0, 1), active))
+          } else {
+            // Selection is at the very end of the document. Select the last character instead.
+            normalizedSelections.push(new vscode.Selection(active, active.translate(0, -1)))
+          }
         }
       } else if (normalizedSelections)
         normalizedSelections.push(selection);
