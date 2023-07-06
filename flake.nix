986d728076b199647d55af3b60e00741786ead4f
Grégoire Geis <opensource@gregoirege.is>
2023-07-07 00:48:19 +0900

    misc: add basic nix flake / direnv setup

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  inputs = {
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
+      perSystem = { config, self', inputs', pkgs, system, ... }: {
+        devShells.default = pkgs.mkShell {
+          name = "dance";
+
+          packages = [
+            pkgs.yarn
+          ];
+        };
+      };
+    };
+}
